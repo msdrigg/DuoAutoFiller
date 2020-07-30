8ccbe55e3eac54ad14cfb7ef32c74f8fc74b53f4
msdrigg <m.scott.driggers@gmail.com>
2020-07-30 07:36:13 -0400

    Updated everything. Currently still not working, but we can interact with duo now

--- filler2.js
+++ filler2.js
@@ -1,37 +1,23 @@
 var default_user = "msdrigg";
 var default_psw = "7Ax2dijSEycAD9QCYDYBt2pnh2kaJFhg";
 
-var duo_submission_complete = false;
-var duo_submission_started = false;
+var duo_submission_state = "loading_doc";
 
 document.body.style.border = "5px solid red";
+var mduo_document = null;
 
-var duo_submission_state = "loading_doc";
-var mduo_document = document;
 
 function dismiss_warnings(duo_document) {
-	console.log("Dismissing warnings");
-	items = duo_document.querySelectorAll("div.message-content button");
-	console.log(items);
-	items.forEach ( element => {
-		console.log("Element found but not clicked");
-		console.log(element.innerHTML);
-		console.log(String(element.innerHTML).contains("Cancel"));
-		if ( element.innerHTML.contains("Dismiss") || element.innerHTML.contains("Cancel")) {
-			element.click();
-			console.log("Element clicked");
-		}
-		else {
-			element.innerHTML = "Yeet";
-		}
-	});
+	duo_document.querySelectorAll("div.message-content button.medium-or-smaller").foreach ( element => {
+	if ( element.innerHTML.contains("Dismiss") || element.innerHTML.contains("Cancel")) {
+		element.click();
+	}
 	if (duo_submission_state == "loading_doc" ) {
 		duo_submission_state = "navigating_to_token";
 	}
 }
 
 function navigate_to_token(duo_document) {
-	console.log("navigating to token");
 	duo_submission_state = "executing";
 	// Enter the correct fieldset and begin work
 	var fieldset = duo_document.getElementsByTagName("fieldsets").filter(element => {
@@ -41,7 +27,7 @@ function navigate_to_token(duo_document) {
 	// Select the token option from the dropdown
 	var device_selector = duo_document.getElementsByName("device")[0];
 	var selected_index = -1;
-	device_selector.options.forEach( (option, i) => {
+	device_selector.options.foreach( (option, i) => {
 		if (option.value.contains("token") && selected_index == -1) {
 			selected_index = i;
 		}
@@ -51,7 +37,6 @@ function navigate_to_token(duo_document) {
 }
 
 function enter_token(duo_document, fieldset) {
-	console.log("Entering token");
 	duo_submission_state = "executing";
 	duo_document
 		.getElementById("remember_me_label_text")
@@ -86,14 +71,11 @@ function enter_token(duo_document, fieldset) {
 
 
 var observer = new MutationObserver( function (mutations, me) {
-	console.log("Mutations observed");
-	console.log(duo_submission_state);
 	var attributes_changed = mutations.some(mutation => mutation.type === "attributes");
 	var tree_changed = mutations.some(mutation => mutation.type === "childList");
 	switch (duo_submission_state) {
 		case "executing":
 		dismiss_warnings(mduo_document);
-		break;
 		case "loading_doc":
 		if (tree_changed) {
 			dismiss_warnings(mduo_document);
@@ -110,17 +92,9 @@ var observer = new MutationObserver( function (mutations, me) {
 });
 
 function duo_iframe_loaded () {
-	console.log("Iframe loaded");
-	console.log(duo_iframe)
-	mduo_document = duo_iframe.contentWindow.document;
-	console.log(mduo_document);
-	observer.observe(mduo_document, {
+	mduo_document = duo_iframe.contentDocument || duo_iframe.contentWindow.document;
+	observer.observe(duo_document, {
 		childList: true,
 	    subtree: true
 	});
-	console.log("Stareted observing");
 }
-observer.observe(mduo_document, {
-	childList: true,
-    subtree: true
-});
