b95301ba8d9403af933c18b7f8bad5f3db19980a
msdrigg <m.scott.driggers@gmail.com>
2020-08-30 03:56:08 -0400

    Updated encryption files

--- javascript-es6/encryption.js
+++ javascript-es6/encryption.js
@@ -1,4 +1,6 @@
 //This file handles the encryption and decryption of stored information
+import {pbkdf2, createHash} from 'crypto';
+
 
 async function storeEncryptedKey(encrypted_vault_key) {
     
@@ -8,8 +10,28 @@ async function retrieveEncryptedKey() {
     
 }
 
+async function hashSHA(inputData){
+    // Input data is a buffer that gets hashed one time
+    const shaHash = crypto.createHash('sha256');
+    shaHash.update(inputData)
+    return shaHash.digest('hex');
+}
+
 async function decrypt(input_text, vault_key) {
     
 }
 
-async function 
+async function decrypt(input_text, vault_key) {
+
+}
+
+async function generateVaultKey(username, password) {
+    return new Promise((resolve, reject)) => {
+        crypto.pbkdf2(password, username, 100100, 256, 'sha256', (err, data) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(data);
+        })
+    });
+}
