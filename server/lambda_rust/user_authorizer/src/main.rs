53698b06b9d3dc88b1c2d0773756ecc71934e162
msdrigg <msdrigg@clemson.edu>
2021-03-11 19:55:38 -0500

    Fixed rust compile issue

---
+++ server/lambda_rust/user_authorizer/src/main.rs
@@ -0,0 +1,41 @@
+use lambda_http::{
+    handler, 
+    lambda_runtime::self,
+    IntoResponse, Request, Context
+};
+use serde_json::json;
+
+type Error = Box<dyn std::error::Error + Sync + Send + 'static>;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    lambda_runtime::run(handler(world)).await?;
+    Ok(())
+}
+
+async fn world(request: Request, ctx: Context) -> Result<impl IntoResponse, Error> {
+    // `serde_json::Values` impl `IntoResponse` by default
+    // creating an application/json response
+    Ok(json!({
+    "message": "Go Serverless v1.0! Your function executed successfully!"
+    }))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn world_handles() {
+        let request = Request::default();
+        let expected = json!({
+        "message": "Go Serverless v1.0! Your function executed successfully!"
+        })
+        .into_response();
+        let response = world(request, Context::default())
+            .await
+            .expect("expected Ok(_) value")
+            .into_response();
+        assert_eq!(response.body(), expected.body())
+    }
+}
